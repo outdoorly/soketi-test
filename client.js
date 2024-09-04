a5305987f42f74aeb21925c481640715220c7fc9
Benjamin Kenawell <ben@outdoorly.com>
2024-09-04 07:25:50 -0400

    add user auth, bring express into server for it

--- client.js
+++ client.js
@@ -1,24 +1,28 @@
-import PusherJS from 'pusher-js';
+import PusherJS from "pusher-js";
 
 function log(...args) {
-  console.log('[client]', ...args);
+  console.log("[client]", ...args);
 }
 
-let client = new PusherJS('app-key', {
-  cluster: '',
-  httpHost: '127.0.0.1',
+let client = new PusherJS("app-key", {
+  cluster: "",
+  httpHost: "127.0.0.1",
   httpPort: 6001,
-  wsHost: '127.0.0.1',
+  wsHost: "127.0.0.1",
   wsPort: 6001,
   wssPort: 6001,
   forceTLS: false,
-  enabledTransports: ['ws', 'wss'],
+  enabledTransports: ["ws", "wss"],
+  userAuthentication: {
+    endpoint: "http://localhost:4321/pusher/user-auth",
+  },
 });
 
-client.subscribe('chat-room').bind('message', (message) => {
-  if('sender' in message && 'content' in message)
+client.signin();
+client.user.bind("to-me", (data) => log(data));
+log("signed in");
+client.subscribe("chat-room").bind("message", (message) => {
+  if ("sender" in message && "content" in message)
     log(`${message.sender} says: ${message.content}`);
-  else
-    log('message', message);
+  else log("message", message);
 });
-
--- server.js
+++ server.js
@@ -1,20 +1,50 @@
-import Pusher from 'pusher';
+import Pusher from "pusher";
+import express from "express";
+
+const app = express();
 
 function log(...args) {
-  console.log('[server]', ...args);
+  console.log("[server]", ...args);
 }
 
 const pusher = new Pusher({
-  appId: 'app-id',
-  key: 'app-key',
-  secret: 'app-secret',
-  cluster: '',
-  host: '127.0.0.1',
+  appId: "app-id",
+  key: "app-key",
+  secret: "app-secret",
+  cluster: "",
+  host: "127.0.0.1",
   port: 6001,
-  useTLS: false
+  useTLS: false,
 });
 
-await pusher.trigger('chat-room', 'message', {message: 'hello world!'});
-await pusher.trigger('chat-room', 'message', {sender: 'Ben', content: 'hi there!'});
-log('triggered message');
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
+const USER_ID = "2d017c5a-af67-4cdc-b6c9-76420a44f268";
+app.post("/pusher/user-auth", (req, res) => {
+  const socketId = req.body.socket_id;
+  const userData = { id: USER_ID };
+  const authResp = pusher.authenticateUser(socketId, userData);
+  log("socket id", socketId, "auth resp", authResp);
+  res.send(authResp);
+});
+
+app.get("/user", (req, res) => {
+  log("sending to user", USER_ID);
+  pusher.sendToUser(USER_ID, "to-me", { test: "me" });
+  res.status(200);
+  res.end();
+});
+
+app.get("/message", async (req, res) => {
+  await pusher.trigger("chat-room", "message", { message: "hello world!" });
+  await pusher.trigger("chat-room", "message", {
+    sender: "Ben",
+    content: "hi there!",
+  });
+  log("triggered message");
+  res.status(200);
+  res.end();
+});
+
+app.listen(4321, () => log("listening"));
