a8678a1fe4601fcf0ce87457d0d458f70ddb1251
Benjamin Kenawell <ben@outdoorly.com>
2024-09-10 10:38:26 -0400

    add some command line processing

--- README.md
+++ README.md
@@ -2,7 +2,17 @@
 
 A minimal javascript program to test sending/receiving messages with soketi.
 
-## Running
+## Running Chestnut Script
+
+- assumes soketi is running in chestnut
+
+By default, this command will create a pusher client, sign into the soketi server, listen for messages, then fire a mutation so chestnut sends a message through soketi. If you only want _part of that functionality_ there are `listen` and `send` commands. For more information about each, run:
+
+```bash
+node chesnut-client.js -h
+```
+
+## Running Test Server
 
 ```bash
 > pnpm i
@@ -11,9 +21,10 @@ A minimal javascript program to test sending/receiving messages with soketi.
 ```
 
 After cloning and installing dependencies, `npm start` will run the entire service:
- - from pulling the docker conatiner (if needed)
- - to starting the client/server
- - and tearing it all down at the end (to avoid orphan processes)
+
+- from pulling the docker conatiner (if needed)
+- to starting the client/server
+- and tearing it all down at the end (to avoid orphan processes)
 
 ### Prerequisites
 
--- chestnut-client.js
+++ chestnut-client.js
@@ -9,46 +9,51 @@ function logDir(args) {
   console.dir(args, { depth: 5 });
   console.log(chalk.blue("---"));
 }
+
 const lead = process.env.LEAD || "fc2f683a-4856-49c8-abf2-48dab6dc80b3";
 const conversationId =
   process.env.CONVERSATION_ID || "250232f0-f056-4e99-8b73-3f6f2727154a";
 
-let client = new PusherJS("app-key", {
-  cluster: "",
-  httpHost: "127.0.0.1",
-  httpPort: 6001,
-  wsHost: "127.0.0.1",
-  wsPort: 6001,
-  wssPort: 6001,
-  forceTLS: false,
-  enabledTransports: ["ws", "wss"],
-  userAuthentication: {
-    endpoint: "http://localhost:4000/soketi/user-auth",
-    // The lead needs sent as part of the body
-    params: {
-      lead,
+/** create pusher client to listen to chestnut on conversation id */
+function listen() {
+  let client = new PusherJS("app-key", {
+    cluster: "",
+    httpHost: "127.0.0.1",
+    httpPort: 6001,
+    wsHost: "127.0.0.1",
+    wsPort: 6001,
+    wssPort: 6001,
+    forceTLS: false,
+    enabledTransports: ["ws", "wss"],
+    userAuthentication: {
+      endpoint: "http://localhost:4000/soketi/user-auth",
+      // The lead needs sent as part of the body
+      params: {
+        lead,
+      },
     },
-  },
-});
-
-client.signin();
-client.user.bind("to-me", (data) => log(data));
-log("signed in");
-client
-  .subscribe(`conversation-${conversationId}`)
-  .bind("new-message", (message) => {
-    log("message", message);
   });
 
-/** --- send a message to this conversation --- */
-log("sending message via api");
-const vendorId =
-  process.env.VENDOR_ID || "9a93c317-1fde-479b-b495-fad22def801e";
-const messageBody = process.env.MESSAGE_BODY || "hi there!";
+  client.signin();
+  client.user.bind("to-me", (data) => log(data));
+  log("signed in");
+  client
+    .subscribe(`conversation-${conversationId}`)
+    .bind("new-message", (message) => {
+      log("message", message);
+    });
+}
 
-log("sending body:", messageBody);
+/** send a message to this conversation */
+function sendMessage() {
+  log("sending message via api");
+  const vendorId =
+    process.env.VENDOR_ID || "9a93c317-1fde-479b-b495-fad22def801e";
+  const messageBody = process.env.MESSAGE_BODY || "hi there!";
 
-const query = `
+  log("sending body:", messageBody);
+
+  const query = `
   mutation SendMessage ($input: SendMessageInput!) {
     sendMessage(input: $input) {
       id
@@ -58,20 +63,54 @@ const query = `
       }
 }}
 `;
-fetch("http://localhost:4000/graphql", {
-  method: "POST",
-  headers: { "content-type": "application/json" },
-  body: JSON.stringify({
-    query,
-    operationName: "SendMessage",
-    variables: {
-      input: { lead, vendorId, createChatEvent: { body: messageBody } },
-    },
-  }),
-})
-  .then((resp) => resp.json())
-  .then((resp) => logDir(resp))
-  .catch(() => {
-    console.error("Is chestnut running?");
-    process.exit(1);
-  });
+  fetch("http://localhost:4000/graphql", {
+    method: "POST",
+    headers: { "content-type": "application/json" },
+    body: JSON.stringify({
+      query,
+      operationName: "SendMessage",
+      variables: {
+        input: { lead, vendorId, createChatEvent: { body: messageBody } },
+      },
+    }),
+  })
+    .then((resp) => resp.json())
+    .then((resp) => logDir(resp))
+    .catch(() => {
+      console.error("Is chestnut running?");
+      process.exit(1);
+    });
+}
+
+function all() {
+  listen();
+  sendMessage();
+}
+
+function printHelp() {
+  console.log(`
+A script to run/test Chestnut's Soketi server
+
+COMMANDS
+  listen    start soketi listener
+  send      send test message to chestnut that soketi will pick up
+  all       (default) equivalent of running listen and run.
+
+ENV VARS
+  LEAD             lead of vendor
+  VENDOR_ID        id of vendor lead is in conversation on 
+  CONVERSATION_ID  id of conversation to listen for messages on
+  MESSAGE_BODY     string to send as the message body
+`);
+}
+
+// --- process command line ---
+const args = process.argv;
+args.shift(); // node
+args.shift(); // script file
+if (args.some((x) => /--?h(elp)?/.test(x))) printHelp();
+else if (args.length === 0 || args.includes("all")) all();
+else {
+  if (args.includes("listen")) listen();
+  if (args.includes("send")) sendMessage();
+}
