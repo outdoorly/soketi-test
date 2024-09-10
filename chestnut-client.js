18b3cfa1733f9b01b58595f7e0ef98c33d3fcca8
Benjamin Kenawell <ben@outdoorly.com>
2024-09-10 09:25:39 -0400

    create a script for listening to chestnut

---
+++ chestnut-client.js
@@ -0,0 +1,77 @@
+import PusherJS from "pusher-js";
+import chalk from "chalk";
+
+function log(...args) {
+  console.log(chalk.blue("[client]"), ...args);
+}
+function logDir(args) {
+  console.log(chalk.blue("[client] ---"));
+  console.dir(args, { depth: 5 });
+  console.log(chalk.blue("---"));
+}
+const lead = process.env.LEAD || "fc2f683a-4856-49c8-abf2-48dab6dc80b3";
+const conversationId =
+  process.env.CONVERSATION_ID || "250232f0-f056-4e99-8b73-3f6f2727154a";
+
+let client = new PusherJS("app-key", {
+  cluster: "",
+  httpHost: "127.0.0.1",
+  httpPort: 6001,
+  wsHost: "127.0.0.1",
+  wsPort: 6001,
+  wssPort: 6001,
+  forceTLS: false,
+  enabledTransports: ["ws", "wss"],
+  userAuthentication: {
+    endpoint: "http://localhost:4000/soketi/user-auth",
+    // The lead needs sent as part of the body
+    params: {
+      lead,
+    },
+  },
+});
+
+client.signin();
+client.user.bind("to-me", (data) => log(data));
+log("signed in");
+client
+  .subscribe(`conversation-${conversationId}`)
+  .bind("new-message", (message) => {
+    log("message", message);
+  });
+
+/** --- send a message to this conversation --- */
+log("sending message via api");
+const vendorId =
+  process.env.VENDOR_ID || "9a93c317-1fde-479b-b495-fad22def801e";
+const messageBody = process.env.MESSAGE_BODY || "hi there!";
+
+log("sending body:", messageBody);
+
+const query = `
+  mutation SendMessage ($input: SendMessageInput!) {
+    sendMessage(input: $input) {
+      id
+      body
+      identity { 
+        id 
+      }
+}}
+`;
+fetch("http://localhost:4000/graphql", {
+  method: "POST",
+  headers: { "content-type": "application/json" },
+  body: JSON.stringify({
+    query,
+    operationName: "SendMessage",
+    variables: {
+      input: { lead, vendorId, createChatEvent: { body: messageBody } },
+    },
+  }),
+})
+  .then((resp) => resp.json())
+  .then((resp) => logDir(resp))
+  .catch(() => {
+    console.error("Is chestnut running?");
+    process.exit(1);
+  });
